b2acebb7cbfeb8418686d9444edb060f8ec25030
taoufikOURO <ourotaoufik1@gmail.com>
2025-08-16 10:00:06 +0000

    refactor: clean up formatting and improve code readability in root component

--- app/root.tsx
+++ app/root.tsx
@@ -1,48 +1,49 @@
-
-
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+    useLoaderData,
 } from "@remix-run/react";
 import { ThemeProvider } from "./context/theme";
 import "./tailwind.css";
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: "/votre-feuille-de-style.css" },
+    { rel: "stylesheet", href: "/votre-feuille-de-style.css" },
 ];
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  // Ici votre logique d'authentification
-  const user = { name: "Admin", role: "admin" }; // Exemple
-  return json({ user });
+    // Ici votre logique d'authentification
+    const user = { name: "Admin", role: "admin" }; // Exemple
+    return json({ user });
 }
 
 export default function App() {
-  const { user } = useLoaderData<typeof loader>();
+    const { user } = useLoaderData<typeof loader>();
 
-  return (
-    <html lang="fr">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
+    return (
+        <html lang="fr">
+            <head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <Meta />
+                <Links />
+                <link rel="icon" href="/assets/images/logo_Qapp.jpg" />
+            </head>
+            <body>
+                <ThemeProvider>
+                    <Outlet />
+                </ThemeProvider>
 
-        <ThemeProvider>
-          <Outlet />
-        </ThemeProvider>
-
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+                <ScrollRestoration />
+                <Scripts />
+            </body>
+        </html>
+    );
 }
